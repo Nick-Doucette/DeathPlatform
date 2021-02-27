4e5459314f4263630f8927a542a62dd601ef4f8e
Nick-Doucette <doucettejnick@gmail.com>
2021-02-27 16:07:41 -0400

    Fire Pillar finished

--- DeathPlatform/Assets/Nicks Stuff/Scripts/FirePillar_Controller.cs
+++ DeathPlatform/Assets/Nicks Stuff/Scripts/FirePillar_Controller.cs
@@ -8,13 +8,17 @@ public class FirePillar_Controller : MonoBehaviour
     private float timeRemaining = 5f;
     [SerializeField]
     private float timeRemaining2 = 2f;
-    private bool firing = false;
+    public bool firing = false;
 
     private GameObject sprite;
+    private Animator fireAnim;
 
+    private ActivePlayer ap;
     void Awake()
     {
-        sprite = this.gameObject.transform.Find("Fire").gameObject;
+        sprite = this.gameObject.transform.Find("FireAnim").gameObject;
+        ap = GameObject.FindGameObjectWithTag("ActivePlayer").GetComponent<ActivePlayer>();
+        fireAnim = sprite.GetComponent<Animator>();
     }
 
     void Update()
@@ -52,19 +56,23 @@ public class FirePillar_Controller : MonoBehaviour
     {
         this.gameObject.GetComponent<Collider2D>().enabled = true;
         sprite.GetComponent<SpriteRenderer>().enabled = true;
+        fireAnim.SetBool("Firing", true);
     }
 
     private void StopFire()
     {
         this.gameObject.GetComponent<Collider2D>().enabled = false;
         sprite.GetComponent<SpriteRenderer>().enabled = false;
+        fireAnim.SetBool("Firing", false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player" || collision.tag == "Player2")
         {
-            //ded
+            GameObject collidingPlayer = collision.gameObject;
+            if (collidingPlayer.GetComponent<PlayerMovement>().isActive){ ap.SwitchActivePlayers(); }
+            collidingPlayer.GetComponent<PlayerMovement>().ChangeToPlatform();
         }
     }
 }
