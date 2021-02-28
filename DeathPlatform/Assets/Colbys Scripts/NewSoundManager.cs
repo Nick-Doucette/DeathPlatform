38d0737509a0102dbb8f459c883c59f9a6219779
Benjamin08 <RFLXcolby@gmail.com>
2021-02-28 17:19:55 -0400

    added cutscenes. added background to 1-2

---
+++ DeathPlatform/Assets/Colbys Scripts/NewSoundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewSoundManager : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+
+
+    static bool AudioBegin = false;
+
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (!AudioBegin)
+        {
+            audioSource.Play();
+            DontDestroyOnLoad(gameObject);
+            AudioBegin = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "matts 1-1")
+        {
+            audioSource.Stop();
+            AudioBegin = false;
+        }
+    }
+}
--- DeathPlatform/Assets/Colbys Scripts/StartSounds.cs
+++ DeathPlatform/Assets/Colbys Scripts/StartSounds.cs
@@ -2,16 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class StartSounds : MonoBehaviour
 {
     private AudioSource audioSource;
     public AudioClip buttonHoverSound;
     public AudioClip buttonClickSound;
 
+    static bool AudioBegin = false;
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (!AudioBegin)
+        {
+            audioSource.Play();
+            DontDestroyOnLoad(gameObject);
+            AudioBegin = true;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        
+
     }
 
     // Update is called once per frame
