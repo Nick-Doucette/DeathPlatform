f59de43a523c63602843d6fec4998a1ebd5eea69
Benjamin08 <RFLXcolby@gmail.com>
2021-02-28 12:06:32 -0400

    Title screen sounds, reimport textmeshpro

---
+++ DeathPlatform/Assets/Colbys Scripts/StartSounds.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSounds : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip buttonHoverSound;
+    public AudioClip buttonClickSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonHoverSound()
+    {
+        audioSource.volume = 1f;
+        audioSource.PlayOneShot(buttonHoverSound); 
+    }    
+
+    public void ButtonClickSound()
+    {
+        audioSource.volume = 1f;
+        audioSource.PlayOneShot(buttonClickSound);
+    }
+}
